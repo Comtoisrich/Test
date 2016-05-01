a355aff28f6f6e486bd2aa6e7fbce35c1563efed
comtoisrich@gmail.com <comtoisrich@gmail.com>
2016-04-30 23:50:03 -0400

    Just add text

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
      System.out.println("Hello, world from GIT!");
      System.out.println("Changes after first commit");
      System.out.println("Changes after pushing to the remote");
-     System.out.println("More changes");
+     System.out.println("More and more changes");
 
   }
 }
